058bed29d4f4f46d24f94e4ea4d5387d7ac41c75
Ebiseri Aginighan <mrebis1@gmail.com>
2023-01-27 10:15:45 -0500

    Name added to Hello

--- ScannerAndKeyboard.java
+++ ScannerAndKeyboard.java
@@ -7,6 +7,6 @@ public class ScannerAndKeyboard
 	{	Scanner s = new Scanner(System.in);
 		System.out.print( "Enter your name: "  );
 		String name = s.nextLine();
-		System.out.println( "Hello " + name + "!" );
+		System.out.println( "Hello " + name + "EBIS!" );
 	}
 }
